309b4ea49784a8c2797aed836ca4e6e2dbdb3edf
hangytong <hangytong@gmail.com>
2025-09-29 15:51:47 +0800

    update

--- insider_buy_sell/insider_ticker.py
+++ insider_buy_sell/insider_ticker.py
@@ -12,7 +12,10 @@ if __name__ == "__main__":
 
     all_dfs = []
 
+    print(tickers)
+
     for ticker in tickers:
+        print(f"evaluating {ticker}")
         url = f"http://openinsider.com/screener?s={ticker}&o=&pl=&ph=&ll=&lh=&fd={days}&fdr=&td=0&tdr=&fdlyl=&fdlyh=&daysago=&xp=1&xs=1&vl=&vh=&ocl=&och=&sic1=-1&sicl=100&sich=9999&grp=0&nfl=&nfh=&nil=&nih=&nol=&noh=&v2l=&v2h=&oc2l=&oc2h=&sortcol=0&cnt=100&page=1"
         df = insider_analysis(url)
         if df is not None and not df.empty:
